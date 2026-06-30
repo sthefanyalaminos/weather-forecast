b204a8218c4241bbee8fa8090b3d5505b4eb8a92
Sthefany Alaminos <alaminossthefany2@gmail.com>
2026-06-30 14:54:35 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-document.querySelector("#search").addEventListener("submit", (event) => {
+document.querySelector("#search").addEventListener("submit", async (event) => {
     event.preventDefault();
 
     const cityName = document.querySelector("#city_name").value;
@@ -6,6 +6,14 @@ document.querySelector("#search").addEventListener("submit", (event) => {
     if (!cityName) {
         return showAlert('Por favor, digite o nome de uma cidade.');
     }
+
+    const apiKey = API_KEY;
+    const apiUrl = API_URL;
+
+    const results = await fetch(apiUrl);
+    const json = await results.json();
+
+    
 });
 
 function showAlert(msg) {
