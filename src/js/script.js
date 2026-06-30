b06e1b231188982aee58d64df8ed80faf4d6f24a
Sthefany Alaminos <alaminossthefany2@gmail.com>
2026-06-30 13:54:21 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,13 @@
 document.querySelector("#search").addEventListener("submit", (event) => {
     event.preventDefault();
-};
+
+    const cityName = document.querySelector("#city_name").value;
+
+    if (!cityName) {
+        return showAlert('Por favor, digite o nome de uma cidade.');
+    }
+});
+
+function showAlert(msg) {
+    document.querySelector("#alert").innerHTML = msg;
+}
